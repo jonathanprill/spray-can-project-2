d3803c5ec110f400d6a1a713821f4533700c7b55
NumiBlue <thekindlyblog@gmail.com>
2022-03-29 18:35:51 -0500

    finishes Vote db

--- models/Vote.js
+++ models/Vote.js
@@ -1,2 +1,44 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+
+class Vote extends Model {
+};
+
+Vote.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull:false,
+            references: {
+                model: "user",
+                key: "id"
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "post",
+                key: "id"
+            }
+        },
+        flag: {
+            type: DataTypes.BOOLEAN,   
+        },
+
+
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "vote"
+    }
+)
+
+
+
+module.exports = Comment;
