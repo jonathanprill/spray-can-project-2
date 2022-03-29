4820bbcb0e72eb6d97320fe2ed5a041167516bc7
NumiBlue <thekindlyblog@gmail.com>
2022-03-29 18:01:56 -0500

    adds sequelize tables

--- models/User.js
+++ models/User.js
@@ -10,5 +10,35 @@ class User extends Model {
     }
 };
 
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            taype: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        },
+    
+    }
+)
 
 module.exports = User;
