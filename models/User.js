82ce4a5e2315c499b8e3aba67e300c4f3fbd3f3a
NumiBlue <thekindlyblog@gmail.com>
2022-03-29 17:48:19 -0500

    creates User

--- models/User.js
+++ models/User.js
@@ -1,2 +1,14 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+
+
+const bcrypt = require("bcrypt");
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password)
+    }
+};
+
+
+module.exports = User;
