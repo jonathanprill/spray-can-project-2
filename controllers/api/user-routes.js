e8af04c382f1a19cba3e9ca4f6706d1b69da683b
Brandon Henry <brandonhenry@Ginas-iMac.lan>
2022-03-30 12:49:36 -0500

    sets up user get/post routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -11,3 +11,57 @@ router.get('/', (req, res) => {
         res.status(500),json(err);
     });
 });
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password']},
+        where: {
+            id:req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'description', 'image', 'location', 'created_at'],
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['id']
+                }
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
+            }
+        ]
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.user,
+        email: req.body.email,
+        password: req.body.password
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
