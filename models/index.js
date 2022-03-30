33b70df24f8f8a05b15083837cc66a37f930b37e
NumiBlue <thekindlyblog@gmail.com>
2022-03-29 19:08:16 -0500

    saves User, Post, Comment associations

--- models/index.js
+++ models/index.js
@@ -13,4 +13,34 @@ User.hasMany(Vote, {
     foreignKey: "user_id"
 });
 
+User.belongsToMany(Post, {
+    through: Vote,
+    as: "voted_posts",
+    foreignKey: "post_id"
+});
+
+
+Post.belongsToMany(User, {
+    foreignKey: "user_id"
+});
+Post.belongsToMany(User, {
+    through: Vote,
+    as: "voted_posts",
+    foreignKey: "user_id"
+});
+
+Post.hasMany(Vote, {
+    foreignKey: "post_id"
+});
+
+Post.hasMany(Comemnt, {
+    foreignKey: "post_id"
+});
+
+Comment.belongToUser(Comment, {
+    foreignKey: "user_id"
+});
+
+
+
 module.exports = { User, Post, Comment, Vote };
