99483ba3a71c9bc4cb812542cb791b96270ffb07
Brandon Henry <brandonhenry@Ginas-iMac.lan>
2022-03-29 19:12:54 -0500

    creates vote and comment associations

--- models/index.js
+++ models/index.js
@@ -12,14 +12,12 @@ User.hasMany(Comment, {
 User.hasMany(Vote, {
     foreignKey: "user_id"
 });
-
 User.belongsToMany(Post, {
     through: Vote,
     as: "voted_posts",
     foreignKey: "post_id"
 });
 
-
 Post.belongsToMany(User, {
     foreignKey: "user_id"
 });
@@ -28,19 +26,26 @@ Post.belongsToMany(User, {
     as: "voted_posts",
     foreignKey: "user_id"
 });
-
 Post.hasMany(Vote, {
     foreignKey: "post_id"
 });
-
 Post.hasMany(Comemnt, {
     foreignKey: "post_id"
 });
 
-Comment.belongToUser(Comment, {
+Comment.belongsTo(User, {
     foreignKey: "user_id"
 });
+Comment.belongsTo(Post, {
+    foreignKey: "post_id"
+});
 
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
 
 
 module.exports = { User, Post, Comment, Vote };
