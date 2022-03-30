4af47af27695f39725bffdd9949e65da7e9e47f2
Brandon Henry <brandonhenry@Ginas-iMac.lan>
2022-03-30 11:48:13 -0500

    gets Post, Comment, and User attributes for home route

---
+++ controllers/home-routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id', 
+            'description',
+            'image',
+            'location',
+            'created_at'
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 
+            'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+})
