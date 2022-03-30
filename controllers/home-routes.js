62b3eae9d1e75bc21ad2899f6ed4d1b0849ce1a8
Brandon Henry <brandonhenry@Ginas-iMac.lan>
2022-03-30 11:56:16 -0500

    sets up render from homepage.handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -28,4 +28,12 @@ router.get('/', (req, res) => {
             }
         ]
     })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true}));
+        res.render('homepage', {posts});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
 })
