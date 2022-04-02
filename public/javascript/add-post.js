c7887da0d2a0a2d4c17386dda867047b3ae2aa96
jonyprill <jonathantprill@gmail.com>
2022-04-02 09:00:25 -0500

    adds ability to post a Post

---
+++ public/javascript/add-post.js
@@ -0,0 +1,29 @@
+// The Function that the dashboard uses to create new posts.
+
+async function newFormHandler(event) {
+    event.preventDefault();
+
+    const description = document.querySelector('input[name="post-desc"]').value;
+    const image = document.querySelector('input[name="post-image"]').value;
+    const location = document.querySelector('input[name="post-location"]').value;
+
+    const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({
+            description,
+            image,
+            location
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
