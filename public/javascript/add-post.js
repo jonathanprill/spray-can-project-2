99c84a3be17bc3f754d94a0fbac301a32c6f3436
Brandon Henry <brandonhenry@Ginas-iMac.lan>
2022-04-03 16:23:38 -0500

    adds card image path, date,  and map iframe

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -30,4 +30,4 @@ async function newFormHandler(event) {
     
 }
 
-document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+document.querySelector('#upload-button').addEventListener('submit', newFormHandler);
