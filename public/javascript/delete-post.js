a08e6c8f4264f3eaa2aaa68844337358928b264c
jonyprill <jonathantprill@gmail.com>
2022-04-09 20:31:48 -0500

    adds delete option

---
+++ public/javascript/delete-post.js
@@ -0,0 +1,19 @@
+// button-click handler 
+async function deleteFormHandler(event) {
+    event.preventDefault();
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE'
+    });
+
+    if (response.ok) {
+        document.location.replace('/myposts/');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
